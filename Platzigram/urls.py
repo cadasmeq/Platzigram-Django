541bbd0fc18f14ffee85cb58961063fba780e02b
cris-linux <cadasmeq02@gmail.com>
2020-08-11 17:44:55 -0400

    Se crea perfil basado en modelo de usuario (proxy de auth), creado además super usuario

--- Platzigram/urls.py
+++ Platzigram/urls.py
@@ -1,14 +1,18 @@
+# django
+from django.contrib import admin
 from django.urls import path
+
+# app
 from . import views as local_views
 from posts import views as posts_views
 
 
 urlpatterns = [
-    # personal views
+
+    path('admin/', admin.site.urls),
+
     path('hello', local_views.hello_world),
     path('hi', local_views.hi),
     path('say_hi/<str:name>/<int:age>', local_views.say_hi),
-
-    # app posts views
     path('posts', posts_views.list_posts)
 ]
