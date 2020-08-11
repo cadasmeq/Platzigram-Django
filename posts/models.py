94ef449bfe0de53da3940fb5236fa852c2ec1102
cris-linux <cadasmeq02@gmail.com>
2020-08-11 12:00:04 -0400

    [working] Creado y migrado modelo de usuario para app <posts>

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    email       = models.EmailField
+    password    = models.CharField(max_length=25)
+    
+    first_name  = models.CharField(max_length=25)
+    last_name   = models.CharField(max_length=25)
+
+    bio         = models.TextField()
+
+    birthday    = models.DateField()
+    created     = models.DateTimeField()         
+    modified    = models.DateTimeField()
